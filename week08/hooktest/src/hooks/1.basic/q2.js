cf825056c644b8f79b2d79da18cca2e835db025b
윤국현 <djaudwls7873@gmail.com>
2023-05-13 23:59:14 +0900

    feat/ week08 basic q2, q3

--- week08/hooktest/src/hooks/1.basic/q2.js
+++ week08/hooktest/src/hooks/1.basic/q2.js
@@ -1,15 +1,68 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function Q2() {
-  const arr = [];
+  const inputRef = useRef();
+  const listRef = useRef([]);
+  const pRefColor = useRef();
   const [forceRender, setForceRender] = useState(false);
 
   const onAddList = () => {
+    const value = inputRef.current.value;
+    listRef.current.push(value);
     setForceRender((prev) => !prev);
-    arr.push();
   };
 
-  /* 
+  const onSubmitList = () => {
+    const list = listRef.current;
+    if (list.length === 0) {
+      return;
+    }
+    const items = list.map((item, index) => <li key={index}>{item}</li>);
+    return items;
+  };
+
+  const onChangeColor = () => {
+    const currentColor = pRefColor.current.style.color;
+    if (currentColor === "red") {
+      pRefColor.current.style.color = "black";
+    } else {
+      pRefColor.current.style.color = "red";
+    }
+  };
+
+  return (
+    <>
+      <h1>문제2</h1>
+      <div>
+        <h2>문제 2-1</h2>
+        <p>
+          <input ref={inputRef} />
+        </p>
+        <p>
+          <button onClick={onAddList}>추가</button>
+        </p>
+        <p>
+          <button>제출</button>
+        </p>
+
+        {listRef.current.length === 0 ? (
+          <p>제출된 목록이 없습니다</p>
+        ) : (
+          <ul>{onSubmitList()}</ul>
+        )}
+      </div>
+      <div>
+        <h2>문제 2-2</h2>
+        <p ref={pRefColor}> 이 문구는 아래 버튼을 누르면 색상이 바뀝니다</p>
+        <button onClick={onChangeColor}>변경</button>
+      </div>
+    </>
+  );
+}
+
+export default Q2;
+
+/* 
     문제2
 
     2-1)
@@ -36,35 +89,7 @@ function Q2() {
         p태그의 색상이 다른 색상으로 변경됩니다.
         
         이는 state를 사용하는 것이 가장 올바른 방법이지만
-        어를 사용할 수 없는 어쩔 수 없는 상황에 놓여있습니다.
+        이를 사용할 수 없는 어쩔 수 없는 상황에 놓여있습니다.
 
         따라서 useRef는 사용하여 해당 문구의 색상을 변경해보세요 :)
   */
-
-  return (
-    <>
-      <h1>문제2</h1>
-      <div>
-        <h2>문제 2-1</h2>
-        <p>
-          <input />
-        </p>
-        <p>
-          <button onClick={onAddList}>추가</button>
-        </p>
-        <p>
-          <button>제출</button>
-        </p>
-
-        <p>제출된 목록이 없습니다</p>
-        <ul>{/* -- list -- */}</ul>
-      </div>
-      <div>
-        <h2>문제 2-2</h2>
-        <p> 이 문구는 아래 버튼을 누르면 색상이 바뀝니다</p>
-        <button>변경</button>
-      </div>
-    </>
-  );
-}
-export default Q2;
--- week08/hooktest/src/hooks/1.basic/q3.js
+++ week08/hooktest/src/hooks/1.basic/q3.js
@@ -1,7 +1,55 @@
+import React, { useState, useEffect } from "react";
 import Q3components from "../../components/1.basic/q3components";
 
 function Q3() {
-  /* 
+  const [count, setCount] = useState(0); // 줄넘기 횟수 state
+  const [showComponent, setShowComponent] = useState(false); // Q3components 보이기/숨기기 state
+
+  useEffect(() => {
+    let intervalId; // intervalId를 저장할 변수
+
+    if (showComponent) {
+      // Q3components가 보이는 상태일 때 interval 함수 실행
+      intervalId = setInterval(() => {
+        setCount((prevCount) => prevCount + 1);
+      }, 2000);
+    }
+
+    return () => {
+      clearInterval(intervalId); // 컴포넌트가 unmount될 때 interval 함수 clear
+    };
+  }, [showComponent]);
+
+  const handleStartClick = () => {
+    setShowComponent(true); // Q3components 보이기
+    setCount(0); // 줄넘기 횟수 초기화
+  };
+
+  const handleStopClick = () => {
+    setShowComponent(false); // Q3components 숨기기
+    setCount(0); // 줄넘기 횟수 초기화
+  };
+
+  return (
+    <>
+      <h1>문제3</h1>
+      <div>
+        <p> 줄넘기 횟수 : {count} </p>
+        {showComponent && <Q3components />}
+        <p>
+          <button onClick={handleStartClick}>줄넘기 시작</button>
+        </p>
+        <p>
+          <button onClick={handleStopClick}>줄넘기 중지</button>
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Q3;
+
+/* 
     문제3
     useEffect useState에 관련한 문제입니다
     단체 줄넘기 대회에 출전하였습니다
@@ -21,21 +69,3 @@ function Q3() {
     해당 컴포넌트는 보이지 않아야하며, 줄넘기 횟수도 더이상 증가해서는 안됩니다.
     또한, 줄넘기 횟수는 0으로 고정되어야합니다.
   */
-
-  return (
-    <>
-      <h1>문제3</h1>
-      <div>
-        <p> 줄넘기 횟수 : 0 </p>
-        <Q3components />
-        <p>
-          <button>줄넘기 시작</button>
-        </p>
-        <p>
-          <button>줄넘기 중지</button>
-        </p>
-      </div>
-    </>
-  );
-}
-export default Q3;
