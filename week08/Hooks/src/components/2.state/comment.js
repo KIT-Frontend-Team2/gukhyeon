9d2bb39b4acf19e1874fc8ea8fa7d789570eb6b6
윤국현 <djaudwls7873@gmail.com>
2023-06-03 19:19:33 +0900

    feat/week08 state2 수정;;

---
+++ week08/Hooks/src/components/2.state/comment.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+function Comment({ nickname, content, onDelete, handleUpdateComment }) {
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [editContent, setEditContent] = useState(content);
+
+  const handleSetIsEditMode = () => {
+    if (!isEditMode) return setIsEditMode(true);
+    handleUpdateComment(nickname, content);
+    setIsEditMode(false);
+  };
+
+  const onChange = (e) => {
+    setEditContent(e.target.value);
+  };
+
+  return (
+    <S.CommentItem>
+      <p>
+        작성자: <span>{nickname}</span>
+      </p>
+
+      <span>
+        {isEditMode ? (
+          <textarea value={editContent} onChange={onChange}></textarea>
+        ) : (
+          <p>
+            댓글 내용: <span>{content}</span>{" "}
+          </p>
+        )}
+      </span>
+
+      <button onClick={handleSetIsEditMode}>수정</button>
+      <button onClick={onDelete}>삭제</button>
+    </S.CommentItem>
+  );
+}
+export default Comment;
+
+const CommentItem = styled.li`
+  border: 1px solid #000;
+  margin: 10px;
+`;
+
+const S = {
+  CommentItem,
+};
